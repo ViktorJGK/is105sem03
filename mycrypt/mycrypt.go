4aab53bf36b00c20a1d079fff54196b5b9144ac5
Viktor J.G Kalhovd <viktorkallhovd02@gmail.com>
2023-04-17 11:32:39 +0200

    la til store bokstaver i alfabetet

--- mycrypt/mycrypt.go
+++ mycrypt/mycrypt.go
@@ -1,15 +1,24 @@
 package mycrypt
 
-var ALF_SEM03 []rune = []rune("abcdefghijklmnopqrstuvwxyzæøå0123456789.,:; ")
+import "unicode"
+
+var ALF_SEM03 []rune = []rune("abcdefghijklmnopqrstuvwxyzæøåABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,:; ")
 
 func Krypter(melding []rune, alphabet []rune, chiffer int) []rune {
 	kryptertMelding := make([]rune, len(melding))
 	for i := 0; i < len(melding); i++ {
 		indeks := sokIAlfabetet(melding[i], alphabet)
-		if indeks+chiffer >= len(alphabet) {
-			kryptertMelding[i] = alphabet[indeks+chiffer-len(alphabet)]
+		if indeks == -1 {
+			// Hvis symbol ikke finnes i alfabetet, kopier det direkte til den krypterte meldingen
+			kryptertMelding[i] = melding[i]
 		} else {
-			kryptertMelding[i] = alphabet[indeks+chiffer]
+			if indeks+chiffer >= len(alphabet) {
+				// Hvis indeksen overskrider antall symboler i alfabetet etter å ha lagt til chiffer,
+				// bruk modulo-operatoren for å wrap rundt til starten av alfabetet
+				kryptertMelding[i] = alphabet[indeks+chiffer-len(alphabet)]
+			} else {
+				kryptertMelding[i] = alphabet[indeks+chiffer]
+			}
 		}
 	}
 	return kryptertMelding
@@ -17,10 +26,11 @@ func Krypter(melding []rune, alphabet []rune, chiffer int) []rune {
 
 func sokIAlfabetet(symbol rune, alfabet []rune) int {
 	for i := 0; i < len(alfabet); i++ {
-		if symbol == alfabet[i] {
+		// Bruk unicode.ToLower for å konvertere symbol og alfabet[i] til små bokstaver før sammenligning
+		if unicode.ToLower(symbol) == unicode.ToLower(alfabet[i]) {
 			return i
-			break
 		}
 	}
+	// Returner -1 hvis symbol ikke finnes i alfabetet
 	return -1
 }
