99d194f9eae82f878621e36ead28a2a868e12704
ViktorJGK <vjkalhovd@uia.no>
2023-04-19 14:37:35 +0200

    Gokjent testcase ved å legge inn hele alfabete og endret på ønket på testcase

--- main.go
+++ main.go
@@ -7,14 +7,14 @@ import (
 
 func main() {
 
-    kryptertRune := mycrypt.Krypter([]rune("Kjevik;SN39040;18.03.2022 01:50;6"), mycrypt.ALF_SEM03, 4)
-    kryptertString := string(kryptertRune)
-    fmt.Println(kryptertString)
-  
-    alfLength := len(mycrypt.ALF_SEM03)
- 
-    dekryptertRune := mycrypt.Krypter(kryptertRune, mycrypt.ALF_SEM03, alfLength-4)
-    dekryptertString := string(dekryptertRune)
-    fmt.Println(dekryptertString)
+	kryptertRune := mycrypt.Krypter([]rune("Kjevik;SN39040;18.03.2022 01:50;6"), mycrypt.ALF_SEM03, 4)
+	kryptertString := string(kryptertRune)
+	fmt.Println(kryptertString)
+
+	alfLength := len(mycrypt.ALF_SEM03)
+
+	dekryptertRune := mycrypt.Krypter(kryptertRune, mycrypt.ALF_SEM03, alfLength-4)
+	dekryptertString := string(dekryptertRune)
+	fmt.Println(dekryptertString)
 
 }
--- mycrypt/mycrypt.go
+++ mycrypt/mycrypt.go
@@ -1,24 +1,15 @@
 package mycrypt
 
-import "unicode"
-
-var ALF_SEM03 []rune = []rune("abcdefghijklmnopqrstuvwxyzæøåABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,:; ")
+var ALF_SEM03 []rune = []rune("abcdefghijklmnopqrstuvwxyzæøåABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ0123456789.,:; ")
 
 func Krypter(melding []rune, alphabet []rune, chiffer int) []rune {
 	kryptertMelding := make([]rune, len(melding))
 	for i := 0; i < len(melding); i++ {
 		indeks := sokIAlfabetet(melding[i], alphabet)
-		if indeks == -1 {
-			// Hvis symbol ikke finnes i alfabetet, kopier det direkte til den krypterte meldingen
-			kryptertMelding[i] = melding[i]
+		if indeks+chiffer >= len(alphabet) {
+			kryptertMelding[i] = alphabet[indeks+chiffer-len(alphabet)]
 		} else {
-			if indeks+chiffer >= len(alphabet) {
-				// Hvis indeksen overskrider antall symboler i alfabetet etter å ha lagt til chiffer,
-				// bruk modulo-operatoren for å wrap rundt til starten av alfabetet
-				kryptertMelding[i] = alphabet[indeks+chiffer-len(alphabet)]
-			} else {
-				kryptertMelding[i] = alphabet[indeks+chiffer]
-			}
+			kryptertMelding[i] = alphabet[indeks+chiffer]
 		}
 	}
 	return kryptertMelding
@@ -26,11 +17,10 @@ func Krypter(melding []rune, alphabet []rune, chiffer int) []rune {
 
 func sokIAlfabetet(symbol rune, alfabet []rune) int {
 	for i := 0; i < len(alfabet); i++ {
-		// Bruk unicode.ToLower for å konvertere symbol og alfabet[i] til små bokstaver før sammenligning
-		if unicode.ToLower(symbol) == unicode.ToLower(alfabet[i]) {
+		if symbol == alfabet[i] {
 			return i
+			break
 		}
 	}
-	// Returner -1 hvis symbol ikke finnes i alfabetet
 	return -1
 }
